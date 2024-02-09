81ee17c350280f31137c632652a95e3cb0bf81d4
angela <angmarie0916@gmail.com>
2024-02-09 12:52:46 -0500

    added postgresql

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'djangojokes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jokes',
+        'USER': 'postgres',
+        'PASSWORD': 'hello',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
