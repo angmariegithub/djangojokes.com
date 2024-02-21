02b6451221eedf51380a3b5fe2422f7f77d5a428
angela <angmarie0916@gmail.com>
2024-02-20 20:15:04 -0500

    Job Applications to models

--- jobs/forms.py
+++ jobs/forms.py
@@ -4,6 +4,12 @@ from datetime import datetime
 from django.core.exceptions import ValidationError
 from django.core.validators import URLValidator
 
+from .models import Applicant
+
+def validate_checked(value):
+    if not value:
+        raise ValidationError("Required.")
+    
 def validate_future_date(value):
     if value < datetime.now().date():
         raise ValidationError(
@@ -11,7 +17,7 @@ def validate_future_date(value):
         )
 
 
-class JobApplicationForm(forms.Form):
+class JobApplicationForm(forms.ModelForm):
     first_name = forms.CharField(
         widget=forms.TextInput(attrs={'autofocus': True}))
     last_name = forms.CharField()
@@ -54,4 +60,33 @@ class JobApplicationForm(forms.Form):
     desired_hourly_wage = forms.DecimalField(widget=forms.NumberInput(attrs={'min': '10.00', 'max': '100.00', 'step': '.25'}))
     cover_letter = forms.CharField(widget=forms.Textarea(attrs={'cols': '75', 'rows': '5'}))
     confirmation = forms.BooleanField(
-        label='I certify that the information I have provided is true')
+        label='I certify that the information I have provided is true',
+        validators=[validate_checked])
+    
+    class Meta:
+        model = Applicant
+        fields = (
+            'first_name', 'last_name', 'email', 'website', 'employment_type',
+            'start_date', 'available_days', 'desired_hourly_wage',
+            'cover_letter', 'confirmation', 'job')
+        widgets = {
+            'first_name': forms.TextInput(attrs={'autofocus': True}),
+            'website': forms.TextInput(
+                attrs = {'placeholder':'https://www.example.com'}
+            ),
+            'start_date': forms.SelectDateWidget(
+                attrs = {
+                    'style': 'width: 31%; display: inline-block; margin: 0 1%'
+                },
+                years = range(datetime.now().year, datetime.now().year+2)
+            ),
+            'desired_hourly_wage': forms.NumberInput(
+                attrs = {'min':'10.00', 'max':'100.00', 'step':'.25'}
+            ),
+            'cover_letter': forms.Textarea(attrs={'cols': '100', 'rows': '5'})
+        }
+        error_messages = {
+            'start_date': {
+                'past_date': 'Please enter a future date.'
+            }
+        }
