ee49dfda0a58f873150d946fa9be7409cd68b0c7
angela <angmarie0916@gmail.com>
2024-03-05 14:16:28 -0500

    implemented search parameters

--- jokes/views.py
+++ jokes/views.py
@@ -1,4 +1,5 @@
 import json
+from django.db.models import Q
 from django.http import JsonResponse
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView, UpdateView)
 from django.urls import reverse_lazy
@@ -92,6 +93,12 @@ class JokeListView(ListView):
     def get_queryset(self):
         ordering = self.get_ordering()
         qs = Joke.objects.all()
+
+        if 'q' in self.request.GET: # Filter by search query
+            q = self.request.GET.get('q') 
+            qs = qs.filter(
+                Q(question__icontains=q) | Q(answer__icontains=q)
+            )
 
         if 'slug' in self.kwargs: # Filter by category or tag
             slug = self.kwargs['slug']
