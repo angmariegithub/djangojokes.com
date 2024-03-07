fff6983fafef828f5d156a1b3829bb6402635334
angela <angmarie0916@gmail.com>
2024-03-07 10:17:36 -0500

    last minute admin changes

--- jokes/admin.py
+++ jokes/admin.py
@@ -21,12 +21,18 @@ class JokeAdmin(DjangoJokesAdmin):
     ordering = ['-updated']
     list_filter = ['updated', 'category', 'tags']
     date_hierarchy = 'updated'
+    # Form Attributes
+    autocomplete_fields = ['tags', 'user']
+    radio_fields = { 'category': admin.HORIZONTAL}
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
-            return ('slug', 'created', 'updated')
+            return ('slug', 'created', 'updated', 'vote_summary')
 
         return ()
+    
+    def vote_summary(self, obj):
+        return f'{obj.num_votes} votes. Rating: {obj.rating}.'
 
 @admin.register(JokeVote)
 class JokeVoteAdmin(DjangoJokesAdmin):
@@ -42,6 +48,7 @@ class JokeVoteAdmin(DjangoJokesAdmin):
 class TagAdmin(DjangoJokesAdmin):
     model = Tag
     list_display = ['tag', 'created', 'updated']
+    search_fields = ['tag']
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
